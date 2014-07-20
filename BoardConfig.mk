445bc412d5f5527ad8413ff95cbd669ba6ba2392
andi34 <skate4life@gmx.de>
2014-07-21 00:20:47 +0200

    use cyanogenmod_defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/lt013g/bluetooth
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4x12
-TARGET_KERNEL_CONFIG := custom_lt013g_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_lt013g_defconfig
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := lt013g,lt013gxx,SM-T311
